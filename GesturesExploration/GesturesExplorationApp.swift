824cb1da478e02b48b5121840937f5eea42f563f
Weerawut Chaiyasomboon <weerawut@live.com>
2024-02-23 09:49:20 +0700

    Initial Commit

---
+++ GesturesExploration/GesturesExplorationApp.swift
@@ -0,0 +1,17 @@
+//
+//  GesturesExplorationApp.swift
+//  GesturesExploration
+//
+//  Created by Weerawut Chaiyasomboon on 23/2/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct GesturesExplorationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
